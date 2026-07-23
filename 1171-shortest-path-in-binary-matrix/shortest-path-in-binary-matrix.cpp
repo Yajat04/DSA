cac5e33e2011cdfe28f265218f654ab2cc8997d9
Yajat Sharma <yajat004@gmail.com>
2026-07-23 21:55:09 +0530

    Time: 11 ms (88.85%) | Memory: 25.1 MB (63.86%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,43 +3,41 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        priority_queue < pair < int, pair < int, int >> ,
-                            vector < pair < int, pair < int, int >> > ,
-                            greater < pair < int, pair < int, int >> >
-                        > pq;
-        vector < vector < int >> dist (n, vector < int > (n, INT_MAX));
+        if(n == 1) return 1;
+        
+        queue < pair < int, pair < int, int >>> q;
+        vector < vector < bool >> vis (n, vector < bool > (n));
 
-        dist[0][0] = 1;
-        pq.push({1, {0, 0}});
+        vis[0][0] = 1;
+        q.push({1, {0, 0}});
 
         int dx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
         int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-        while(!pq.empty()){
-            auto top = pq.top();
-            pq.pop();
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
 
-            int d =  top.first;
-            int r =  top.second.first;
-            int c =  top.second.second;
-
-            if(dist[r][c] < d) continue;
-
-            if(r == n-1 && c == n-1) return dist[r][c];
+            int d =  front.first;
+            int r =  front.second.first;
+            int c =  front.second.second;
 
             for(int i = 0; i < 8; i++){
                 int nr = r + dx[i];
                 int nc = c + dy[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0){
-                    if(dist[r][c] + 1 < dist[nr][nc]){
-                        dist[nr][nc] = dist[r][c] + 1;
-                        pq.push({dist[nr][nc], {nr, nc}});
-                    }
+                if(nr == n-1 && nc == n-1) return d + 1;
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0 
+                    && vis[nr][nc] == 0){
+                    vis[nr][nc] = 1;
+                    q.push({d + 1, {nr, nc}});
                 }
             }
         }
-        
+
         return -1;
     }
 };
+
+//Not optimal since it has constant edge wt hence normaal bfs is enough
