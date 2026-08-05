40cf5455ed4321be0108cbb3199f3668a4f814ca
Yajat Sharma <yajat004@gmail.com>
2026-08-06 02:52:29 +0530

    Time: 2078 ms (5.12%) | Memory: 452.2 MB (5.12%) - LeetSync

--- 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
+++ 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
@@ -3,14 +3,19 @@ class Solution {
     vector <int> result;
     unordered_map<string, int> dp;
     void solve(int idx, int m, vector <int> &people_skills, vector <int> temp, int mask){
-        if(idx == m){
-            if(mask == target_mask && 
-                (result.size() == 0 || result.size() > temp.size())) result = temp;
+        if(mask == target_mask){ 
+        //If already completed taking all reqskills then no need to go further, temp for this would be updated and will get dtored in result
+        //if(idx == m) not required here as it is the valid path or accepted path
+            if(result.size() == 0 || (result.size() > temp.size())) result = temp;
             return;
         }
 
+        if(idx == m) return; //if path isnt valid and it hits the end of vector
+
+        //prune using results
         if(result.size() != 0 && result.size() <= temp.size()) return;
 
+        //Prune using dp
         string key = to_string(idx) + "_" + to_string(mask);
         if(dp.find(key) != dp.end()){
             if(dp[key] <= temp.size()) return;
@@ -22,7 +27,7 @@ class Solution {
         solve(idx + 1, m, people_skills, temp, mask);
 
         //take
-        if(people_skills[idx] != 0){ //or mask | people_skills[idx] != mask , means p[idx] isnt empty and have some skills o/w it wont make any sense to take  it
+        if(mask | people_skills[idx] != mask){ //means p[idx] isnt empty and have some skills o/w it wont make any sense to take it, also if it do have skills and it wont update the mask as it already covers the same skills earlier, there isnt any need to take it
             temp.push_back(idx);
             solve(idx + 1, m, people_skills, temp, (mask | people_skills[idx]));
             temp.pop_back();
@@ -40,9 +45,9 @@ public:
         }
 
         vector <int> people_skills;
-        for(auto p : people){
+        for(const auto &p : people){
             int p_mask = 0;
-            for(auto s : p){
+            for(const auto &s : p){
                 p_mask = p_mask | (1 << skills[s]);
             }
             people_skills.push_back(p_mask);
