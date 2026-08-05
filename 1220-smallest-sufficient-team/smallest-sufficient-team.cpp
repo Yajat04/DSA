71cd88890d3b8620e56942f8979cd23713890b7a
Yajat Sharma <yajat004@gmail.com>
2026-08-06 02:36:50 +0530

    Time: 1829 ms (5.12%) | Memory: 400.2 MB (5.12%) - LeetSync

---
+++ 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
@@ -0,0 +1,57 @@
+class Solution {
+    int target_mask;
+    vector <int> result;
+    unordered_map<string, int> dp;
+    void solve(int idx, int m, vector <int> &people_skills, vector <int> temp, int mask){
+        if(idx == m){
+            if(mask == target_mask && 
+                (result.size() == 0 || result.size() > temp.size())) result = temp;
+            return;
+        }
+
+        if(result.size() != 0 && result.size() <= temp.size()) return;
+
+        string key = to_string(idx) + "_" + to_string(mask);
+        if(dp.find(key) != dp.end()){
+            if(dp[key] <= temp.size()) return;
+        }
+
+        dp[key] = temp.size();
+
+        //not_take
+        solve(idx + 1, m, people_skills, temp, mask);
+
+        //take
+        if(people_skills[idx] != 0){ //or mask | people_skills[idx] != mask , means p[idx] isnt empty and have some skills o/w it wont make any sense to take  it
+            temp.push_back(idx);
+            solve(idx + 1, m, people_skills, temp, (mask | people_skills[idx]));
+            temp.pop_back();
+        }
+    }
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        int n = req_skills.size();
+        int m = people.size();
+
+        //Tranform re_skills in bits
+        unordered_map <string, int> skills;
+        for(int i = 0; i < n; i++){
+            skills[req_skills[i]] = i;
+        }
+
+        vector <int> people_skills;
+        for(auto p : people){
+            int p_mask = 0;
+            for(auto s : p){
+                p_mask = p_mask | (1 << skills[s]);
+            }
+            people_skills.push_back(p_mask);
+        }
+
+        vector <int> temp;
+        target_mask = pow(2, n) - 1;
+        solve(0, m, people_skills, temp, 0);
+
+        return result;
+    }
+};
