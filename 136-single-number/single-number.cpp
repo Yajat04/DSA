f8b35316845ad5961105efb37e1674cbe1cfab87
Yajat Sharma <yajat004@gmail.com>
2026-08-08 22:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.42%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xorr = nums[0];
+        for(int i = 1; i < n; i++){
+            xorr = xorr ^ nums[i];
+        }
+
+        return xorr;
+    }
+};
