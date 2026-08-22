8b32d04f4a33e1452f27119e4698d1010b15a1e1
Yajat Sharma <yajat004@gmail.com>
2026-08-22 22:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (41.18%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,8 +1,12 @@
 class Solution {
+    //substring, as it wpuld require some bracket containing continous chars
+    //reminds of window
+    //dynamic window as we need to find largest of the substring
+    //move left ahead upto that point such that freq of the repeating el resets to 1
 public:
     int lengthOfLongestSubstring(string s) {
         int left = 0;
-        vector <int> last(128, -1); //s consists of English letters, digits, symbols and spaces, if only lowercase letters then 26 would have been enough
+        vector <int> last(128, -1);
         int maxi = 0;
         for(int right = 0; right < s.size(); right++){
             if(last[s[right]] >= left) left = last[s[right]] + 1;
