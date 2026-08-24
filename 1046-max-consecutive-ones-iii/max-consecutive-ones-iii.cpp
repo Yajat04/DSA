be99ca0f0f29133a54f340a14d45b33cfba33991
Yajat Sharma <yajat004@gmail.com>
2026-08-24 18:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (37.11%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    //Sliding window as two ptr and window come into play
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
@@ -7,12 +8,11 @@ public:
         queue<int> q;
         for(int right = 0; right < n; right++){
             if(nums[right] == 0){
-                if(k == 0){
+                if(k == 0){ //As queue cant handle it, we have to restart the strings after zeros
                     left = right + 1;
                     continue;
                 }
-
-                if(k != 0 && q.size() == k){
+                else if(q.size() == k){
                     int idx = q.front();
                     q.pop();
                     left = idx + 1;
