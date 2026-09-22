17bbf83a4435a740fb7674c8c843f5880ea5322d
Yajat Sharma <yajat004@gmail.com>
2026-09-22 19:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.90%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
+    //Logic is if there arnt any missing nu =m then for inc sorted order of nums
+    //num "i+1" must be present at ith index
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
-        int cnt = arr[0] - 1;
-        if(cnt >= k) return k;
 
-        for(int i = 1; i < n; i++){
-            cnt += arr[i] - arr[i-1] - 1;
-            if(cnt >= k){
-                int last = arr[i-1];
-                int prevcnt = cnt -(arr[i] - arr[i-1] - 1);
-                return last + k - prevcnt;
-            }
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing < k) low = mid+1;
+            else high = mid-1;
+
         }
         
-        return arr[n-1] + k - cnt;
+        return low + k;
     }
 };
